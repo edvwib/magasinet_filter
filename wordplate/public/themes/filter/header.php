a6db9fc77e7c6693b97503c396b9561dfa2f6ebd
Edvin Wiberg <edde275@gmail.com>
2018-09-19 21:56:17 +0200

    add serviceworker

--- wordplate/public/themes/filter/header.php
+++ wordplate/public/themes/filter/header.php
@@ -2,8 +2,11 @@
 <html <?php language_attributes(); ?>>
 <head>
   <meta charset="<?php bloginfo('charset'); ?>">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-  <meta name="theme-color" content="#6d9aea">
+  <meta name="theme-color" content="#5ab10d">
+  <link rel="shortcut icon" href="themes/filter/assets/favicon.ico" type="image/x-icon">
+  <link rel="manifest" href="themes/filter/assets/manifest.json">
 
   <?php wp_head(); ?>
 </head>
---
+++ wordplate/workbox-config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  "globDirectory": "public/themes/filter/assets",
+  "globPatterns": [
+    "**/*.{ico,js,css,svg,ttf,woff,woff2,json,gif,png,jpg,xml}"
+  ],
+  "swDest": "public/themes/filter/assets/scripts/sw.js",
+};
