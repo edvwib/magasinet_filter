56c0954ece9b95c7ef2c0475699621423915af19
Edvin Wiberg <edde275@gmail.com>
2018-09-20 09:53:37 +0200

    only register serviceworker if in production mode

--- wordplate/resources/assets/scripts/app.js
+++ wordplate/resources/assets/scripts/app.js
@@ -1,8 +1,13 @@
 'use strict';
 
-if ('serviceWorker' in navigator) {
-  // Use the window load event to keep the page load performant
-  window.addEventListener('load', () => {
-    navigator.serviceWorker.register('themes/filter/assets/scripts/sw.js');
+if (process.env.NODE_ENV === 'production') {
+  // Check that service workers are registered
+  window.addEventListener('load', function () {
+    if ('serviceWorker' in navigator) {
+      // Use the window load event to keep the page load performant
+      window.addEventListener('load', () => {
+        navigator.serviceWorker.register('themes/filter/assets/scripts/sw.js');
+      });
+    }
   });
 }
