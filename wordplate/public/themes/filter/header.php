75b422163cb3c86a260ce6bb7539d82a2a9cae7e
Edvin Wiberg <edde275@gmail.com>
2018-09-23 12:06:34 +0200

    fix incorrect manifest link

--- wordplate/public/themes/filter/header.php
+++ wordplate/public/themes/filter/header.php
@@ -5,8 +5,8 @@
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
   <meta name="theme-color" content="#5ab10d">
-  <link rel="shortcut icon" href="themes/filter/assets/favicon.ico" type="image/x-icon">
-  <link rel="manifest" href="themes/filter/assets/manifest.json">
+  <link rel="shortcut icon" href="/themes/filter/assets/favicon.ico" type="image/x-icon">
+  <link rel="manifest" href="/themes/filter/assets/manifest.json">
 
   <?php wp_head(); ?>
 </head>
