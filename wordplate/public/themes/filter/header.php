a77281c5173e53da16264bb3c7ead2a4554b57e4
Edvin Wiberg <edde275@gmail.com>
2018-09-25 09:34:55 +0200

    fix indentation

--- wordplate/public/themes/filter/header.php
+++ wordplate/public/themes/filter/header.php
@@ -1,12 +1,12 @@
 <!DOCTYPE html>
 <html <?php language_attributes(); ?>>
 <head>
-  <meta charset="<?php bloginfo('charset'); ?>">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-  <meta name="theme-color" content="#5ab10d">
-  <link rel="shortcut icon" href="/themes/filter/assets/favicon.ico" type="image/x-icon">
-  <link rel="manifest" href="/themes/filter/assets/manifest.json">
+    <meta charset="<?php bloginfo('charset'); ?>">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="theme-color" content="#5ab10d">
+    <link rel="shortcut icon" href="/themes/filter/assets/favicon.ico" type="image/x-icon">
+    <link rel="manifest" href="/themes/filter/assets/manifest.json">
 
   <?php wp_head(); ?>
 </head>
