bfcc0e13adc1523cc4d69b5c35a078ec22aff8e7
Edvin Wiberg <edde275@gmail.com>
2018-09-28 18:21:45 +0200

    display articleFont settings labels with another font until the wanted one has loaded

--- wordplate/workbox-config.js
+++ wordplate/workbox-config.js
@@ -1,7 +1,7 @@
 module.exports = {
   "globDirectory": "public/themes/filter/assets",
   "globPatterns": [
-    "**/*.{ico,js,css,svg,ttf,woff,woff2,json,gif,png,jpg,xml}"
+    "**/*.{ico,js,css,svg,ttf,otf,woff,woff2,json,gif,png,jpg,xml}"
   ],
   "swDest": "public/themes/filter/assets/scripts/sw.js",
 };
