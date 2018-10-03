9ddc575d00a6409f82468bcc8105c975465671a1
Edvin Wiberg <edde275@gmail.com>
2018-10-03 16:15:06 +0200

    added footer

--- wordplate/public/themes/filter/footer.php
+++ wordplate/public/themes/filter/footer.php
@@ -1,3 +1,22 @@
+    <footer>
+       <div class="logo"></div>
+       <div class="links">
+           <a href="">OM FILTER</a>
+           <a href="">MAGASINET</a>
+           <a href="">PRENUMERERA</a>
+           <a href="">ANNONSERA</a>
+           <a href="">MEDARBETARE</a>
+           <a href="">KONTAKT</a>
+           <a href="">REPORTAGE</a>
+           <a href="">SHOP</a>
+       </div>
+       <div class="media">
+            <a href="https://twitter.com" class="twitter"></a>
+            <a href="https://facebook.com" class="facebook"></a>
+            <a href="https://instagram.com" class="instagram"></a>
+        </div>
+    </footer>
+
     <?php wp_footer(); ?>
 </body>
 </html>
