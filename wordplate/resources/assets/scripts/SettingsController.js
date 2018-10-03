f3d12d144cb6ce7b57e7b58e4a5ebb0b336e8eb4
Edvin Wiberg <edde275@gmail.com>
2018-10-03 10:41:52 +0200

    fix settingsButton click event

--- wordplate/resources/assets/scripts/SettingsController.js
+++ wordplate/resources/assets/scripts/SettingsController.js
@@ -1,12 +1,10 @@
 import SettingsViewController from './SettingsViewController.js';
-import SettingsButtonController from './SettingsButtonController.js'
 import FontController from './FontController.js';
 import LayoutController from './LayoutController.js';
 import ColorController from './ColorController.js';
 
 export default class SettingsController{
   constructor(){
-    new SettingsViewController();
 
     this.settingsButton = document.querySelector('.articlePost .settingsButton');
     if (this.settingsButton) {
@@ -14,12 +12,66 @@ export default class SettingsController{
       this.settingsClose = this.settings.querySelector('.close');
       this.setUpListener(this.settingsButton);
       this.setUpListener(this.settingsClose);
-    }
 
-    new SettingsButtonController();
-    new FontController();
-    new LayoutController();
-    new ColorController();
+      this.move = this.move.bind(this);
+      this.end = this.end.bind(this);
+      this.update = this.update.bind(this);
+
+      this.dragging = false;
+      this.X = window.innerWidth - 15;
+      this.Y = 287;
+      this.windowHeight = window.innerHeight;
+      this.windowWidth = window.innerWidth;
+      this.setUpMoveListeners();
+
+      new SettingsViewController();
+      new FontController();
+      new LayoutController();
+      new ColorController();
+    }
+  }
+
+  setUpMoveListeners() {
+    this.settingsButton.addEventListener('touchmove', this.move);
+    this.settingsButton.addEventListener('touchend', this.end);
+  }
+
+  move(e) {
+    if (!e.target.classList.contains('settingsButton'))
+      return;
+
+    this.dragging = true;
+    requestAnimationFrame(this.update);
+
+    this.X = e.clientX || e.touches[0].clientX;
+    this.Y = e.clientY || e.touches[0].clientY;
+
+    e.preventDefault();
+  }
+
+  end() {
+    this.dragging = false;
+    this.settingsButton.style.right = 'unset';
+    if (this.X > this.windowWidth / 2) {
+      this.X = this.windowWidth - this.settingsButton.clientWidth - 15;
+      this.settingsButton.style.left = `${this.X}px`;
+    } else {
+      this.X = 15;
+      this.settingsButton.style.left = `${this.X}px`;
+    }
+  }
+
+  update() {
+    if (!this.dragging)
+      return;
+
+    requestAnimationFrame(this.update);
+
+    this.settingsButton.style.left = `${Math.abs(this.X - this.settingsButton.clientWidth / 2)}px`;
+    this.settingsButton.style.top = `${this.Y - this.settingsButton.clientHeight / 2}px`;
+
+    document.querySelector('.settings').style.top = `${this.Y - this.settingsButton.clientHeight / 2}px`;
+
   }
 
   setUpListener(e){
